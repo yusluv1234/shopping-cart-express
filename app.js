78b12455b418634fa33aac6bde9c155399a90221
yusluv1234 <matthewtully124@gmail.com>
2021-04-12 23:37:04 +0100

    stop and do another thing

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let expressHbs = require('express-handlebars');
 
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-let expressHbs = require('express-handlebars');
 
 let app = express();
 
 // view engine setup
-app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'layout.hbs'}));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
